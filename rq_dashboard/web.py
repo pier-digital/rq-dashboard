ec40f28920faef8e81a6cd1dcd106307a662f595
Mauricio Banduk <mauricio.banduk@pier.digital>
2021-12-15 11:57:59 -0300

    Fix typo

--- rq_dashboard/web.py
+++ rq_dashboard/web.py
@@ -276,7 +276,7 @@ def queues_overview(instance_number):
         render_template(
             "rq_dashboard/queues.html",
             current_instance=instance_number,
-            instance_list=obfuscated_redis_url(current_app.config.get("RQ_DASHBOARD_REDIS_URL")),
+            instance_list=obfuscated_redis_urls(current_app.config.get("RQ_DASHBOARD_REDIS_URL")),
             queues=Queue.all(),
             rq_url_prefix=url_for(".queues_overview"),
             rq_dashboard_version=rq_dashboard_version,
@@ -297,7 +297,7 @@ def workers_overview(instance_number):
         render_template(
             "rq_dashboard/workers.html",
             current_instance=instance_number,
-            instance_list=obfuscated_redis_url(current_app.config.get("RQ_DASHBOARD_REDIS_URL")),
+            instance_list=obfuscated_redis_urls(current_app.config.get("RQ_DASHBOARD_REDIS_URL")),
             workers=Worker.all(),
             rq_url_prefix=url_for(".queues_overview"),
             rq_dashboard_version=rq_dashboard_version,
@@ -333,7 +333,7 @@ def jobs_overview(instance_number, queue_name, registry_name, per_page, page):
         render_template(
             "rq_dashboard/jobs.html",
             current_instance=instance_number,
-            instance_list=obfuscated_redis_url(current_app.config.get("RQ_DASHBOARD_REDIS_URL")),
+            instance_list=obfuscated_redis_urls(current_app.config.get("RQ_DASHBOARD_REDIS_URL")),
             queues=Queue.all(),
             queue=queue,
             per_page=per_page,
@@ -359,7 +359,7 @@ def job_view(instance_number, job_id):
         render_template(
             "rq_dashboard/job.html",
             current_instance=instance_number,
-            instance_list=obfuscated_redis_url(current_app.config.get("RQ_DASHBOARD_REDIS_URL")),
+            instance_list=obfuscated_redis_urls(current_app.config.get("RQ_DASHBOARD_REDIS_URL")),
             id=job.id,
             rq_url_prefix=url_for(".queues_overview"),
             rq_dashboard_version=rq_dashboard_version,
